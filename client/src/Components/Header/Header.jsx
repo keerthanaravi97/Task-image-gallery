52e01c03471d7f3523a32e9fe069f770f842c1f9
keerthanaravi97 <139036832+keerthanaravi97@users.noreply.github.com>
2023-08-01 15:20:02 +0530

    client

---
+++ client/src/Components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Heading,
+} from "@chakra-ui/react";
+
+export default function Header() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={"gray.500"} px={4}>
+        <Heading color={"#fff"} textAlign={"center"} p={5}>
+          Image Gallery
+        </Heading>
+      </Box>
+    </>
+  );
+}
